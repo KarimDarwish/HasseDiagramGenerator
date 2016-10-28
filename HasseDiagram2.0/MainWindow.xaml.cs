f3924b99d8999f8fa1f31384369539dabc5782d2
Karim <karimdarwish12@gmail.com>
2016-10-28 09:06:33 +0200

    Code cleanup

--- HasseDiagram2.0/MainWindow.xaml.cs
+++ HasseDiagram2.0/MainWindow.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -7,79 +7,78 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using GraphVizWrapper;
-using System.IO;
 using GraphVizWrapper.Commands;
 using GraphVizWrapper.Queries;
-
 
 namespace HasseDiagram2._0
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
     public partial class MainWindow : Window
     {
-        private Point origin;
-        private Point start;
-        private GraphGeneration wrapperForGraph;
+        private readonly GraphGeneration _wrapperForGraph;
+        private Point _origin;
+        private Point _start;
+
         public MainWindow()
         {
             InitializeComponent();
-            #region "bs for zoom"
-            TransformGroup group = new TransformGroup();
-            ScaleTransform xform = new ScaleTransform();
+
+            #region "zooming"
+
+            var group = new TransformGroup();
+            var xform = new ScaleTransform();
             group.Children.Add(xform);
-            TranslateTransform tt = new TranslateTransform();
+            var tt = new TranslateTransform();
             group.Children.Add(tt);
             img_graph.RenderTransform = group;
             img_graph.MouseWheel += image_MouseWheel;
             img_graph.MouseLeftButtonDown += image_MouseLeftButtonDown;
             img_graph.MouseLeftButtonUp += image_MouseLeftButtonUp;
             img_graph.MouseMove += image_MouseMove;
+
             #endregion
 
             var getStartProcessQuery = new GetStartProcessQuery();
             var getProcessStartInfoQuery = new GetProcessStartInfoQuery();
-            var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery, getStartProcessQuery);
+            var registerLayoutPluginCommand = new RegisterLayoutPluginCommand(getProcessStartInfoQuery,
+                getStartProcessQuery);
 
             var wrapper = new GraphGeneration(getStartProcessQuery,
-                                              getProcessStartInfoQuery,
-                                              registerLayoutPluginCommand);
-            wrapperForGraph = wrapper;
-           
-
+                getProcessStartInfoQuery,
+                registerLayoutPluginCommand);
+            _wrapperForGraph = wrapper;
         }
-        private void GetGraph(GraphGeneration wrapper)
+
+        private void GetGraph(IGraphGeneration wrapper)
         {
-            String[] set = txt_vars.Text.Split(',');
-            StringBuilder sb = new StringBuilder();
+            var set = txt_vars.Text.Split(',');
+            var sb = new StringBuilder();
             sb.Append("digraph{");
             for (int i = 0, max = 1 << set.Length; i < max; i++)
             {
-                List<string> newList = new List<string>();
-                for (int j = 0; j < set.Length; j++)
+                var newList = new List<string>();
+                for (var j = 0; j < set.Length; j++)
                 {
-                    int isList = (int)i & (1 << j); if (isList > 0)
-                    {
+                    var isList = i & (1 << j);
+                    if (isList > 0)
                         newList.Add(set[j]);
-                    }
                 }
                 if (newList.Count != set.Length)
-                {
-                    printLinks(newList, set, sb);
-                }
+                    PrintLinks(newList, set, sb);
             }
             sb.Append("}");
-            byte[] output = wrapper.GenerateGraph(sb.ToString(), Enums.GraphReturnType.Png);
-            BitmapImage img = LoadImage(output);
-            this.Width = img.Width + 20;
-            this.Height = img.Height + 20;
+            var output = wrapper.GenerateGraph(sb.ToString(), Enums.GraphReturnType.Png);
+            var img = LoadImage(output);
+            Width = img.Width + 20;
+            Height = img.Height + 20;
             img_graph.Source = LoadImage(output);
         }
+
         private static BitmapImage LoadImage(byte[] imageData)
         {
-            if (imageData == null || imageData.Length == 0) return null;
+            if ((imageData == null) || (imageData.Length == 0)) return null;
             var image = new BitmapImage();
             using (var mem = new MemoryStream(imageData))
             {
@@ -95,42 +94,39 @@ namespace HasseDiagram2._0
             return image;
         }
 
-        private static void printLinks(List<string> list, String[] arr, StringBuilder sb)
+        private static void PrintLinks(List<string> list, string[] arr, StringBuilder sb)
         {
-            for (int i = 0; i < arr.Length; i++)
+            foreach (var value in arr)
             {
-                String value = arr[i];
                 if (list.Contains(value) == false)
                 {
-                    List<string> newList = new List<string>();
+                    var newList = new List<string>();
                     newList.AddRange(list);
                     newList.Add(value);
-                    sb.Append(GetListAsString(newList)+ " -> " + GetListAsString(list)  + "[dir=back];"
-                            + " \n");
+                    sb.Append(GetListAsString(newList) + " -> " + GetListAsString(list) + "[dir=back];"
+                                                  + " \n");
                 }
             }
         }
-        private static String GetListAsString(List<string> set)
+
+        private static string GetListAsString(List<string> set)
         {
             set.Sort();
             if (set.Count == 0)
-            {
                 return "\"{}\"";
-            }
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
             sb.Append("\"{");
-            for (int i = 0; i < set.Count; i++)
+            for (var i = 0; i < set.Count; i++)
             {
-                String value = set[i];
+                var value = set[i];
                 sb.Append(value);
                 if (i != set.Count - 1)
-                {
                     sb.Append(",");
-                }
             }
             sb.Append("}\"");
             return sb.ToString();
         }
+
         private void image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             img_graph.ReleaseMouseCapture();
@@ -140,26 +136,30 @@ namespace HasseDiagram2._0
         {
             if (!img_graph.IsMouseCaptured) return;
 
-            var tt = (TranslateTransform)((TransformGroup)img_graph.RenderTransform).Children.First(tr => tr is TranslateTransform);
-            Vector v = start - e.GetPosition(border);
-            tt.X = origin.X - v.X;
-            tt.Y = origin.Y - v.Y;
+            var tt =
+                (TranslateTransform)
+                ((TransformGroup) img_graph.RenderTransform).Children.First(tr => tr is TranslateTransform);
+            var v = _start - e.GetPosition(border);
+            tt.X = _origin.X - v.X;
+            tt.Y = _origin.Y - v.Y;
         }
 
         private void image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             img_graph.CaptureMouse();
-            var tt = (TranslateTransform)((TransformGroup)img_graph.RenderTransform).Children.First(tr => tr is TranslateTransform);
-            start = e.GetPosition(border);
-            origin = new Point(tt.X, tt.Y);
+            var tt =
+                (TranslateTransform)
+                ((TransformGroup) img_graph.RenderTransform).Children.First(tr => tr is TranslateTransform);
+            _start = e.GetPosition(border);
+            _origin = new Point(tt.X, tt.Y);
         }
 
         private void image_MouseWheel(object sender, MouseWheelEventArgs e)
         {
-            TransformGroup transformGroup = (TransformGroup)img_graph.RenderTransform;
-            ScaleTransform transform = (ScaleTransform)transformGroup.Children[0];
+            var transformGroup = (TransformGroup) img_graph.RenderTransform;
+            var transform = (ScaleTransform) transformGroup.Children[0];
 
-            double zoom = e.Delta > 0 ? .2 : -.2;
+            var zoom = e.Delta > 0 ? .2 : -.2;
             transform.ScaleX += zoom;
             transform.ScaleY += zoom;
         }
@@ -167,12 +167,7 @@ namespace HasseDiagram2._0
         private void txt_vars_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
-            {
-                GetGraph(wrapperForGraph);
-            }
+                GetGraph(_wrapperForGraph);
         }
-
-      
     }
 }
-
